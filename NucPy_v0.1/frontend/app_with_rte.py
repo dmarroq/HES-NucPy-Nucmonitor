befded6c91534d0f53284e465e892b38a7040b29
dmarroq_haya <d.marroquin@hayaenergy.com>
2023-09-25 16:38:54 +0200

    Added app.py without rte call, need to check on computer that lets me test on streamlit if it works

--- NucPy_v0.1/frontend/app_with_rte.py
+++ NucPy_v0.1/frontend/app_with_rte.py
@@ -28,8 +28,7 @@ def mongo_unavs_call(user_input_start_date, user_input_end_date, user_input_past
     )
 
     db = client["data"]
-    collection_past_unavs = db["unavs"]
-    collection_unavs =db["unavs_update"]
+    collection = db["unavs"]
 
     start_date = f"{user_input_start_date}T00:00:00"
     end_date = f"{user_input_end_date}T23:59:59"
@@ -57,13 +56,9 @@ def mongo_unavs_call(user_input_start_date, user_input_end_date, user_input_past
         }
     ]
 
-    result1 = list(collection_past_unavs.aggregate(pipeline))
-    result2 = list(collection_unavs.aggregate(pipeline))
-    
-    # Merge the two lists of JSON results
-    merged_result = result1 + result2
+    result = collection.aggregate(pipeline)
 
-    return merged_result
+    return list(result)
 
 # --------------------------------------------------------------------------------------- #
 
@@ -97,7 +92,120 @@ def add_total(data):
 
 # --------------------------------------------------------------------------------------- #
 
-def nuc_monitor(usr_start_date, usr_end_date, past_date, mongo_db_data):
+# This file will simply connect to the rte and get the data directly from there
+
+# Function to create an authentication token. This token is then used in the HTTP requests to the API for authentication.
+# It is necessary to receive data from RTE.
+def get_oauth():
+    # ID from the user. This is encoded to base64 and sent in an HTTP request to receive the oauth token.
+    # This ID is from my account (RMP). However, another account can be created in the RTE API portal and get another ID.
+    joined_ID = '057e2984-edb3-4706-984b-9ea0176e74db:dc9df9f7-9f91-4c7a-910c-15c4832fb7bc'
+    b64_ID = base64.b64encode(joined_ID.encode('utf-8'))
+    b64_ID_decoded = b64_ID.decode('utf-8')
+    
+    # Headers for the HTTP request
+    headers = {'Content-Type': 'application/x-www-form-urlencoded',
+               'Authorization': f'Basic {b64_ID_decoded}'}
+    api_url = 'https://digital.iservices.rte-france.com/token/oauth/'
+    # Call to the API and if successful, the response will be 200.
+    response = requests.post(api_url, headers=headers)
+    
+    # When positive response, the token is retrieved
+    data = response.json()
+    oauth = data['access_token']
+    
+    return(oauth)
+
+# --------------------------------------------------------------------------------------- #
+
+# This function does severall calls to the RTE API (because maximum time between start_date and end_date is 1 month) 
+# the argument past_photo is a boolean (True, False) that indicates if we want to make a photo from the past or not
+# However, the past_photo part and past_date is not yet implemented.
+def get_unavailabilities(usr_start_date, usr_end_date):
+    oauth = get_oauth()
+    print("Get Oauth done")
+    date_type = 'APPLICATION_DATE'
+    
+    # Current year/month/day/hour/minute/second is calculated for the last call to the API. For instance, if today is 05/05/2023,
+    # the last call of the API will be from 01/05/2023 to 05/05/2023 (+current hour,minute,second). 
+    current_datetime = datetime.datetime.now()
+    # current_year = current_datetime.strftime('%Y')
+    # current_month = current_datetime.strftime('%m')
+    # current_day = current_datetime.strftime('%d')
+    # current_hour = current_datetime.strftime('%H')
+    # current_minute = current_datetime.strftime('%M')
+    # current_second = current_datetime.strftime('%S')
+    
+    # Headers for the HTTP request
+    headers = {'Host': 'digital.iservices.rte-france.com',
+               'Authorization': f'Bearer {oauth}'
+        }
+    
+    # the responses object is where we are going to store all the responses from the API.
+    # Initially, current_datetime is included to know when we have called the API and all the
+    # individual results of the API (because each call is Maz 1 month) are stored in responses["results"]
+    responses = {"current_datetime": current_datetime.strftime("%m/%d/%Y, %H:%M:%S"),
+                 "results":[]
+        }
+
+    # --------------------------- HERE HAVE TO GET THE RANGE OF DATES FROM START AND END AND PUT THEM INTO LIST --------------------------- #
+    # Convert start_date and end_date to datetime objects
+    usr_start_date = str(usr_start_date)
+    usr_end_date = str(usr_end_date)
+    start_date_obj = datetime.datetime.strptime(usr_start_date, "%Y-%m-%d").date()
+    end_date_obj = datetime.datetime.strptime(usr_end_date, "%Y-%m-%d").date()
+    # start_date_obj = usr_start_date
+    # end_date_obj = usr_end_date
+    # Initialize lists to store years and months
+    years = []
+    months = []
+
+    # Generate the range of years and months
+    current_date = start_date_obj
+    while current_date <= end_date_obj:
+        years.append(current_date.year)
+        months.append(current_date.month)
+        current_date += datetime.timedelta(days=1)
+
+    # Remove duplicates from the lists
+    years = list(set(years))
+    months = list(set(months))
+    years.sort()
+    months.sort()
+    print(years)
+    print(months)
+   # --------------------------- HERE HAVE TO GET THE RANGE OF DATES FROM START AND END AND PUT THEM INTO LIST --------------------------- #
+
+    # Loop to call the API all the necessary times.
+    for i in range(len(years)):
+        for j in range(len(months)): 
+            # start_year and start_month of the current call to the API
+            start_year = years[i]
+            start_month = months[j]
+            # start_date is constructed. Now we only need to construct the end_date.
+            start_date = f'{start_year}-{start_month}-01T00:00:00%2B02:00'
+
+            if True:
+                # Calculate the number of days in the current month
+                _, num_days = monthrange(int(start_year), int(start_month))
+                end_date = f'{start_year}-{start_month}-{num_days}T23:59:59%2B02:00'
+                    
+                print(f'start date is {start_date}')
+                print(f'end date is {end_date}')
+                
+                # Call to the API
+                api_url = f'https://digital.iservices.rte-france.com/open_api/unavailability_additional_information/v4/generation_unavailabilities?date_type={date_type}&start_date={start_date}&end_date={end_date}'
+
+                response = requests.get(api_url, headers=headers)
+                json_response = response.json()
+                responses["results"].append(json_response)
+    # print(responses)
+    return responses
+
+# --------------------------------------------------------------------------------------- #
+
+
+def nuc_monitor(usr_start_date, usr_end_date, past_date, mongo_db_data, rte_data):
     # # Slightly changed metadata to fit the data from the RTE API: ST-LAURENT B 2 --> ST LAURENT 2, ....
 
     plants_metadata = {"BELLEVILLE 1": 1310.0, "BELLEVILLE 2": 1310.0, "BLAYAIS 1": 910.0, "BLAYAIS 2": 910.0, 
@@ -114,6 +222,44 @@ def nuc_monitor(usr_start_date, usr_end_date, past_date, mongo_db_data):
                    "PENLY 2": 1330.0, "ST ALBAN 1": 1335.0, "ST ALBAN 2": 1335.0, "ST LAURENT 1": 915.0, 
                    "ST LAURENT 2": 915.0, "TRICASTIN 1": 915.0, "TRICASTIN 2": 915.0, "TRICASTIN 3": 915.0, 
                    "TRICASTIN 4": 915.0, "FESSENHEIM 1": 880.0, "FESSENHEIM 2": 880.0}
+
+    # --------------------- INITIAL DATA CLEANING FOR RTE DATA ------------------------ #  
+    # unav_API = rte_data.json()
+    # rte_stuff = get_unavailabilities(usr_start_date, usr_end_date)
+    # rte_stuff = get_rte_data(usr_start_date, usr_end_date)
+    unav_API = rte_data
+    # print(unav_API)
+    # Store the unavailabilities in a list
+    unavailabilities = []
+    print("Unav")
+    for unavailabilities_API in unav_API['results']:
+        try:
+            unavailabilities.extend(unavailabilities_API.get('generation_unavailabilities', []))
+        except:
+            print('There was an error')
+            # print(unavailabilities_API)
+    rte_df = pd.DataFrame(unavailabilities)
+
+
+    def unpack_values(row):
+        if isinstance(row["values"], list):
+            for key, value in row["values"][0].items():
+                row[key] = value
+        return row
+    # Apply the function to each row in the DataFrame
+    rte_df = rte_df.apply(unpack_values, axis=1)
+
+    # Drop the original "values" column
+    rte_df.drop("values", axis=1, inplace=True)
+
+    # Unpack the unit column
+    rte_df2 = pd.concat([rte_df, pd.json_normalize(rte_df['unit'])], axis=1)
+    rte_df2.drop('unit', axis=1, inplace=True)
+
+
+    rte_nuclear_unav = rte_df2[(rte_df2["production_type"] == "NUCLEAR")]
+
+    # --------------------- INITIAL DATA CLEANING FOR RTE DATA ------------------------ #    
 
 
     # --------------------- INITIAL DATA CLEANING FOR MONGO DATA ------------------------ #    
@@ -145,12 +291,21 @@ def nuc_monitor(usr_start_date, usr_end_date, past_date, mongo_db_data):
     mongo_df2 = mongo_df_result
     mongo_df2.rename(columns=lambda col: col.replace('unit.', ''), inplace=True)
 
+    
+
     # --------------------- INITIAL DATA CLEANING FOR MONGO DATA ------------------------ #   
 
     # Make the two dataframes have the same columns
     mongo_unavs = mongo_df2.copy()
     mongo_unavs.drop(columns="type", inplace=True)
 
+    rte_unavs = rte_nuclear_unav.copy()
+    rte_unavs.drop(columns="type", inplace=True)
+
+    # Merge dataframes
+    column_order = mongo_unavs.columns
+    # print(column_order)
+    merged_df = pd.concat([mongo_unavs[column_order], rte_unavs[column_order]], ignore_index=True)
     # merged_df['updated_date'] = merged_df['updated_date'].astype(str)
 
 # --------------------------- HERE IS THE CHANGE TO GET ONLY ACTIVE OR ACTIVE AND INACTIVE --------------------------- #
@@ -162,7 +317,7 @@ def nuc_monitor(usr_start_date, usr_end_date, past_date, mongo_db_data):
     past_date_str = str(past_date)
     current_datetime_str = current_datetime.strftime("%Y-%m-%d")
 
-    nuclear_unav = mongo_unavs.copy()[(mongo_unavs.copy()["production_type"] == "NUCLEAR") & (mongo_unavs.copy()["updated_date"] <= past_date_str)]
+    nuclear_unav = merged_df.copy()[(merged_df.copy()["production_type"] == "NUCLEAR") & (merged_df.copy()["updated_date"] <= past_date_str)]
 
     # if photo_date == True:
     #     nuclear_unav = merged_df.copy()[(merged_df.copy()["production_type"] == "NUCLEAR") & (merged_df.copy()["updated_date"] <= past_date_str)]
@@ -356,7 +511,11 @@ def nuc_monitor(usr_start_date, usr_end_date, past_date, mongo_db_data):
     return json_data
     # -------------------------------------------------
 
-
+@st.cache_data
+def get_rte_data(start_date, end_date):
+    rte_data = get_unavailabilities(start_date, end_date)
+    print(rte_data)
+    return rte_data
 @st.cache_data
 def get_mongodb_data(start_date, end_date, past_date):
     database_data = mongo_unavs_call(start_date, end_date, past_date)
@@ -365,7 +524,8 @@ def get_mongodb_data(start_date, end_date, past_date):
 @st.cache_data
 def get_nucmonitor_data(start_date, end_date, past_date):
     mongo = get_mongodb_data(start_date, end_date, past_date)
-    response_nucmonitor = nuc_monitor(start_date, end_date, past_date, mongo)
+    rte = get_rte_data(start_date, end_date)
+    response_nucmonitor = nuc_monitor(start_date, end_date, past_date, mongo, rte)
     # nucmonitor_data = response_nucmonitor.json()
     # nucmonitor_json = json.loads(nucmonitor_data)
     print(response_nucmonitor)
